34100f15f94b5792bf061a57921b155832ea1aa1
bynx <138079781+sicr2000@users.noreply.github.com>
2023-10-07 22:43:25 +0000

    trying to make second navbar, have to continue

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -28,3 +28,30 @@ export const Navbar = () => {
     </nav>
   );
 };
+
+export const NavbarUser = () => {
+  return (
+    <nav className="navbar fixed-top" style={{ backgroundColor: "black", width: "100%" }}>
+      <div className="container">
+        <Link to="/">
+          <img
+            src={logoZusset}
+            style={{ width: "200px", height: "100px", objectFit: "cover" }}
+          />
+        </Link>
+        <div className="row ml-auto gx-3">
+          <div className="col">
+            <Link to="/register">
+              Profile picture
+            </Link>
+          </div>
+          <div className="col">
+            <Link to="/">
+              <button className="btn btn-light">Logout</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
